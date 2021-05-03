32abc73e619f688796069dc049799147ee64fcea
Amit Saluja <asaluja@wundermancommerce.com>
2021-05-03 17:54:57 +0530

    Some low level Changes #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
     this.checkIsWinner();
     if(this.isWinner){
         this.toastr.success(this.winMsg);
-        this.reloadGame();
+		setTimeout(this.reloadGame, 3000);
     	return;
     }
   }
